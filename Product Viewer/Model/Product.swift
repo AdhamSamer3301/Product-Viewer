5c3f2e6806e8d1f9bc9ec8c987fc92015dae08a3
Adham <adhamsamer3301@gmail.com>
2023-06-03 00:49:47 +0300

    Model Added

--- Product Viewer/Model/Product.swift
+++ Product Viewer/Model/Product.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+// MARK: - Product
+struct Product {
+    let product: ProductClass?
+    let productMerchants: [ProductMerchant]?
+}
+
+// MARK: - ProductClass
+struct ProductClass {
+    let id: String?
+    let name: String?
+    let description: String?
+    let price: String?
+    let imageUrl: String?
+}
+
+// MARK: - ProductMerchant
+struct ProductMerchant {
+    let merchant: Merchant?
+    let merchantProduct: MerchantProduct?
+    let productMerchant: SingleProductMerchant?
+}
+
+// MARK: - Merchant
+struct Merchant {
+    let id: String?
+    let name: String?
+}
+
+// MARK: - MerchantProduct
+struct MerchantProduct {
+    let id: String?
+    let price: String?
+    let upc: String?
+    let sku: String?
+    let buyUrl: String?
+    let discountPercent: String?
+}
+
+// MARK: - SingleProductMerchant
+struct SingleProductMerchant {
+    let id: String?
+    let productId: String?
+    let upc: String?
+    let sku: String?
+    let created: String?
+    let modified: String?
+    let multipleProductsPerPage: String?
+}
