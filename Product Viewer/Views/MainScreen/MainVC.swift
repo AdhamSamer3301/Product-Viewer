d0b93652a058b2bbaf4af23c13220ca738067041
Adham <adhamsamer3301@gmail.com>
2023-06-03 22:35:24 +0300

    Simple Search Added

--- Product Viewer/Views/MainScreen/MainVC.swift
+++ Product Viewer/Views/MainScreen/MainVC.swift
@@ -5,13 +5,18 @@
 //  Created by Adham Samer on 30/05/2023.
 //
 
-import UIKit
-import Reachability
 import CoreData
+import Reachability
+import UIKit
 
 class MainVC: UIViewController {
-    @IBOutlet var searchBar: UISearchBar!
-    @IBOutlet weak var noDataIMg: UIImageView!
+    @IBOutlet var searchBar: UISearchBar! {
+        didSet {
+            searchBar.delegate = self
+        }
+    }
+
+    @IBOutlet var noDataIMg: UIImageView!
     @IBOutlet var productsCollectionView: UICollectionView! {
         didSet {
             productsCollectionView.dataSource = self
@@ -24,43 +29,45 @@ class MainVC: UIViewController {
     var vm: ViewModel = ViewModel()
     var coreDataMngr = CoreDataManager.getInstance()
     var allProducts: [Product]? = []
-    var coreProducts:[NSManagedObject] = []
-    var reachability:Reachability? = try? Reachability()
-    var isNetworkAvailable:Bool = true
-    
+    var searchProducts: [Product]? = []
+    var coreProducts: [NSManagedObject] = []
+    var searchCoreProducts: [NSManagedObject] = []
+    var reachability: Reachability? = try? Reachability()
+    var isNetworkAvailable: Bool = true
+
     override func viewDidLoad() {
         super.viewDidLoad()
         coreProducts = coreDataMngr.fetchFromCoreData()
+        searchCoreProducts = coreDataMngr.fetchFromCoreData()
         if reachability?.connection != .unavailable {
             vm.getData(url: .products)
             vm.bindDataToVC = { () in
                 DispatchQueue.main.async {
                     self.allProducts? = self.vm.products
+                    self.searchProducts? = self.vm.products
                     self.productsCollectionView.reloadData()
                 }
             }
             isNetworkAvailable = true
-        }else{
+        } else {
             isNetworkAvailable = false
             showAlert(title: "No Internet", msg: "Connect to internet to get full experience") { _ in }
             if coreProducts.count == 0 && allProducts?.count == 0 {
                 noDataIMg.isHidden = false
             }
         }
-        
-        
     }
 }
 
-//MARK: CollectionView Functions
+// MARK: CollectionView Functions
+
 extension MainVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if isNetworkAvailable{
+        if isNetworkAvailable {
             return allProducts?.count ?? 0
-        }else{
+        } else {
             return coreProducts.count
         }
-        
     }
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -70,15 +77,14 @@ extension MainVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollec
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "mainCVCell", for: indexPath) as! MainCVCell
-        
-        
+
         if isNetworkAvailable {
             guard let product = allProducts?[indexPath.row] else { return cell }
             if !coreDataMngr.foundInCore(itemToSearch: product.Product?.id ?? "") {
                 coreDataMngr.saveToCoreData(item: product.Product)
             }
-            return cell.configureCell(item:product)
-        }else{
+            return cell.configureCell(item: product)
+        } else {
             let product = coreProducts[indexPath.row]
             cell.productName.text = product.value(forKey: "name") as? String
             cell.productPrice.text = "\(product.value(forKey: "price") as? String ?? "")$"
@@ -86,8 +92,6 @@ extension MainVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollec
             cell.productImage.image = UIImage(named: "404Image")
             return cell
         }
-       
-        
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -101,15 +105,45 @@ extension MainVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollec
     }
 }
 
-//MARK: Rendering
-extension MainVC {
+// MARK: Rendering
+
+extension MainVC: UISearchBarDelegate {
     func showAlert(title: String, msg: String, handler: @escaping (UIAlertAction?) -> Void) {
         let alert = UIAlertController(title: title, message: msg, preferredStyle: UIAlertController.Style.alert)
-        
+
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { action in
             handler(action)
         }))
-        
+
         present(alert, animated: true, completion: nil)
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if isNetworkAvailable {
+            allProducts = []
+            if searchText.isEmpty {
+                allProducts = searchProducts
+            }
+
+            for product in searchProducts ?? [] {
+                if product.Product!.name!.lowercased().contains(searchText.lowercased()) {
+                    allProducts?.append(product)
+                }
+            }
+
+        } else {
+            coreProducts = []
+            if searchText.isEmpty {
+                coreProducts = searchCoreProducts
+            }
+            
+            for product in searchCoreProducts {
+                let productName:String = product.value(forKey: "name") as! String
+                if productName.lowercased().contains(searchText.lowercased()) {
+                    coreProducts.append(product)
+                }
+            }
+        }
+        productsCollectionView.reloadData()
     }
 }
