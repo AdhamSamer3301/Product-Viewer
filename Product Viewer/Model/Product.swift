b5dab7405073423d627e21af2e8df0a88c2e9e79
Adham Samer <48602871+AdhamSamer3301@users.noreply.github.com>
2023-06-04 01:59:17 +0300

    Update Product.swift

--- Product Viewer/Model/Product.swift
+++ Product Viewer/Model/Product.swift
@@ -7,13 +7,6 @@
 
 import Foundation
 
-// This file was generated from JSON Schema using quicktype, do not modify it directly.
-// To parse the JSON, add this file to your project and do:
-//
-//   let product = try? JSONDecoder().decode(Product.self, from: jsonData)
-
-import Foundation
-
 // MARK: - Product
 struct Product: Codable {
     var Product: ProductClass?
